123c63c5ebd7ed4cfbe76222ff30c456020f0d65
vguillet <victor.guillet@protonmail.com>
2025-04-04 14:08:48 +0200

    update

---
+++ maaf_config/maaf_config.py
@@ -0,0 +1,23 @@
+
+# >>>>>>>>>>>> ROS CONFIGURATION FILE
+from .ros_pubsub_config import *
+
+# >>>>>>>>>>>> FRAMEWORK CONFIGURATION FILE
+ALLOCATION_METHOD = "I-CBBA"
+
+DEBUG = False
+
+# >>>>>>>>>> RUN MODES DEFINITION
+SIM = 0
+OPERATIONAL = 1
+
+RUN_MODE = OPERATIONAL
+
+if RUN_MODE == OPERATIONAL:
+    pass
+
+elif RUN_MODE == SIM:
+    from .sim_config import *
+
+# >>>>>>>>>> ORGANISATION CONFIGURATION FILE
+organisation_file_path = "icare_alloc_config/__cache/__CoHoMa_organisation_model_v1.json"
---
+++ maaf_config/sim_config.py
@@ -0,0 +1,15 @@
+# ----------------- NODE CONFIGURATION -----------------
+FLEET_MSG_UPDATE_TIMER = 0.1
+SIM_EPOCH_MIN_DURATION = .5
+# SIM_EPOCH_MIN_DURATION = 1.
+
+# ----------------- SIMULATION CONFIGURATION -----------------
+NO_TASK = 0
+ACTION_1 = 1
+ACTION_2 = 2
+
+EVEN = 0
+RANDOM = 1
+
+release_spread = EVEN
+release_ration = EVEN
