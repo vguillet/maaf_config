ddc42f4ee1de896dae3cbff3e067e7e506d8b387
vguillet <victor.guillet@protonmail.com>
2025-04-14 17:46:04 +0200

    update

--- maaf_config/maaf_config.py
+++ maaf_config/maaf_config.py
@@ -21,3 +21,5 @@ elif RUN_MODE == SIM:
 
 # >>>>>>>>>> ORGANISATION CONFIGURATION FILE
 organisation_file_path = "src/icare_alloc_config/icare_alloc_config/__cache/__CoHoMa_organisation_model_v1.json"
+
+organisation_folder_path = "src/icare_alloc_config/icare_alloc_config"
